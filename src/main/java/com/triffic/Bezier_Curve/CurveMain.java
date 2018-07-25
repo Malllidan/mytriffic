f05daf54126ba5dec177218b2077df94d19851ae
Malllidan <478910581@qq.com>
2018-07-25 20:49:17 +0800

    第四次：增加一个输出各方向来车流量以及当前方向前往其他方向流量的分部计算函数

--- src/main/java/com/triffic/Bezier_Curve/CurveMain.java
+++ src/main/java/com/triffic/Bezier_Curve/CurveMain.java
@@ -1,5 +1,47 @@
 package com.triffic.Bezier_Curve;
 
+import com.triffic.JavaTool.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CurveMain {
-    
+    public static void main(String...args){
+        String sql="i056";
+        String sql_tsc="tsclane";
+        String date1="2017-05-24 0:0:0";
+        String date2="2017-05-25 0:0:0";
+        List<CarData> mycarlist=new ArrayList<>();
+        List<TscLaneData> mytsclanelist=new ArrayList<>();
+        List<TscLaneData> myidtsclanelist=new ArrayList<>();
+        Map<String,List<CarData>> mycarmap=new HashMap<>();
+        Map<String,Float> myflowmap=new HashMap<>();
+        try {
+            MysqlLink.Connect();
+            MysqlLink_Car mysqllink_car= new MysqlLink_Car();
+            mysqllink_car.Select(sql,date1,date2);
+            mycarlist=mysqllink_car.GetCarDataList();
+            mysqllink_car.ResultClose();
+            mysqllink_car.StatetClose();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        try{
+            MysqlLink_Tsclane mysqllink_tsclane=new MysqlLink_Tsclane();
+            mysqllink_tsclane.Select_NoDate(sql_tsc);
+            mytsclanelist=mysqllink_tsclane.GetTscLaneDataList();
+            myidtsclanelist=TscLaneData.GetIdTscLaneDataList(mytsclanelist,56);
+            mycarmap=CurveFlow.DirectionSwitch(mycarlist,myidtsclanelist);
+            myflowmap=CurveFlow.AllFlowCalculate(mycarmap);
+
+
+            System.out.println(myflowmap.get("flow4base"));
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/main/java/com/triffic/Bezier_Curve/CurvePicture.java
@@ -0,0 +1,9 @@
+package com.triffic.Bezier_Curve;
+
+public class CurvePicture {
+    public static void main(String...args){
+
+    }
+
+
+}
